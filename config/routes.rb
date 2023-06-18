61171e2a938d3c2acb06774b4261945b69bfe105
Xavier Castro Rozas <x.castrorozas@gmail.com>
2023-06-18 17:55:28 -0400

    primer commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/projects'
+  get 'pages/contact'
+  root 'pages#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
